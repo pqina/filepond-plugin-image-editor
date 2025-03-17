886915ee0e64f537c5b01a315704c4f780c0b5e6
Rik <rik@pqina.nl>
2025-03-17 15:04:09 +0100

    fix default export error

--- src/FilePondPluginImageEditor.d.ts
+++ src/FilePondPluginImageEditor.d.ts
@@ -1,6 +1,12 @@
 // @ts-ignore
 import { FilePondOptions } from 'filepond';
 
+// this fixes issue with TypeScript no default export error
+declare module '@pqina/filepond-plugin-image-editor' {
+    const plugin: any;
+    export default plugin;
+}
+
 declare module 'filepond' {
     export interface FilePondOptions {
         /** Enable or disable image editor */
